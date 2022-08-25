ba7058d4212c2c1bf3c193d04aa83bbc5496422a
Johnson.Stephen1@mayo.edu <m141127@mforgehn4.mayo.edu>
2022-08-25 15:45:47 -0500

    fixing build type again

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ class pre_build(build_ext):
                     "--prefix", hdf5_dir,
                     "--enable-shared=no",
                     "--with-pic=yes",
-                    "--build=arm64"
+                    "--build=arm"
             ])
 
             subprocess.check_call(["make"])
